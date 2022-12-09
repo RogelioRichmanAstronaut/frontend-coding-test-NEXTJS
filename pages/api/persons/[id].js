27798c848404876b16b9850c9ab8b173445814bb
RogelioRichmanAstronaut <56854119+RogelioRichmanAstronaut@users.noreply.github.com>
2022-12-09 09:02:03 -0500

    v1
    final

--- pages/api/persons/[id].js
+++
@@ -1,9 +0,0 @@
-// export default async function handler(req, res){
-//     const { method, query:{id} } = req;
-
-//     if (method === "GET"){
-//         try{
-//             const persons = await
-//         }
-//     }
-// }
